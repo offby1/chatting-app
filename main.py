3ee27510d45011d4b516a8dcd5830400e4fb13f0
username-3-3 <kamosiakgabo@gmail.com>
2024-08-19 00:27:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
@@ -8,8 +8,47 @@ app.config["SECRET_KEY"] = "weufhweuf"
 socketio = SocketIO(app)
 
 @app.route("/", methods = ["POST", "GET"])
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            break
+
+    return code
+
 def home():
+    session.clear()
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
+        if not name:
+            return render_template("home.html", error="ENTER NAME", code=code, name=name)
+
+        if join != False and not code:
+            return render_template("home.html", error="ENTER CODE", code=code, name=name)
+
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages": []}
+        elif code not in rooms:
+            return render_template("home.html", error="ROOM DOESN'T EXIST", code=code, name=name)
+
+        session["room"] = room
+        session["name"] = name
+        return redirect(url_for("room"))
+
     return render_template("home.html")
+
+@app.route("/room")
+def room():
+    return render_template("room.html")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
