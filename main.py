4a51e7ae84ec967dd96490cadc82226605a35f0d
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-08-18 16:11:19 -0700

    Quick hacks to get it working
    http://localhost:5000/9 => TNSTKDXBM

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-from flask import Flask, render_template, request, session, redirect, url_for
-from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
+
+from flask import Flask, redirect, render_template, request, session, url_for
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "weufhweuf"
 socketio = SocketIO(app)
 
-@app.route("/", methods = ["POST", "GET"])
-def generate_unique_code(length):
-    while True:
-        code = ""
-        for _ in range(length):
-            code += random.choice(ascii_uppercase)
 
-        if code not in rooms:
-            break
+@app.route("/<length>", methods=["POST", "GET"])
+def generate_unique_code(length):
+    length = int(length)
+
+    code = ""
+    for _ in range(length):
+        code += random.choice(ascii_uppercase)
 
     return code
+
 
 def home():
     session.clear()
@@ -30,15 +31,12 @@ def home():
         if not name:
             return render_template("home.html", error="ENTER NAME", code=code, name=name)
 
-        if join != False and not code:
+        if join and not code:
             return render_template("home.html", error="ENTER CODE", code=code, name=name)
 
         room = code
-        if create != False:
+        if not create:
             room = generate_unique_code(4)
-            rooms[room] = {"members": 0, "messages": []}
-        elif code not in rooms:
-            return render_template("home.html", error="ROOM DOESN'T EXIST", code=code, name=name)
 
         session["room"] = room
         session["name"] = name
@@ -46,9 +44,11 @@ def home():
 
     return render_template("home.html")
 
+
 @app.route("/room")
 def room():
     return render_template("room.html")
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
